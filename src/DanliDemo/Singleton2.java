71f0fe5ab1976b0bc723fc8037cc7d468d68499a
momoka <454768497@qq.com>
2018-01-12 13:52:13 +0800

    单例-懒汉和饿汉练习

---
+++ src/DanliDemo/Singleton2.java
@@ -0,0 +1,15 @@
+package DanliDemo;
+
+public class Singleton2 {
+    private Singleton2(){
+
+    }
+    private static Singleton2 instance;
+
+    public static Singleton2 getInstance() {
+        if(instance==null){
+            instance=new Singleton2();
+        }
+        return instance;
+    }
+}
